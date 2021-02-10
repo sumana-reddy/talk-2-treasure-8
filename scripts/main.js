640ef9362076f71df5159970a54e5b7ef16c9d9b
Yashwanth <yashwanthdamera6@gmail.com>
2021-02-10 08:56:09 -0600

    speech synthesis done

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,37 @@
-window.addEventListener('load', main);
+import data from './speech.js'
+
+let colorElement1 = document.getElementById("status");
+let colorElement2 = document.getElementById("status1");
 
 function main() {
     console.log('Page is fully loaded');
 }
 
+window.addEventListener('load', main);
+colorElement1.addEventListener('click', onClickSquareBox1);
+colorElement1.addEventListener('touch', onClickSquareBox1);
+colorElement2.addEventListener('click', onClickSquareBox2);
+colorElement2.addEventListener('touch', onClickSquareBox2);
+
+
+
 async function onClickSquareBox1() {
 
+    data.forEach(function(value){
+        document.getElementById("status").innerHTML = value.status_message;
+        let utterance = new SpeechSynthesisUtterance(value.status_message);
+        speechSynthesis.speak(utterance);
+    });
 }
 
 async function onClickSquareBox2() {
     
+    data.forEach(function(value){
+        document.getElementById("status1").innerHTML = value.status1_message;
+        let utterance = new SpeechSynthesisUtterance(value.status1_message);
+        speechSynthesis.speak(utterance);
+    });
 }
+
+
+
---
+++ scripts/speech.js
@@ -0,0 +1,4 @@
+export default [
+    {status_message :"This is color Brown."},
+    {status1_message : "This is color Orange."}
+]
