1486a1d5f220164e4189c98128085a60b08039d2
Yashwanth <yashwanthdamera6@gmail.com>
2021-02-10 06:08:56 -0600

    Initial commit

---
+++ scrips/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', main);
+
+function main() {
+    console.log('Page is fully loaded');
+}
+
+async function onClickSquareBox1() {
+
+}
+
+async function onClickSquareBox2() {
+    
+}
