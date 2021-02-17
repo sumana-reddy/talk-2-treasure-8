2d20c98119dbb40850f17976cb0255d861d4c607
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-02-17 07:46:20 -0600

    created getlocation

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import data from './speech.js'
+import locationsArray from './location.js';
 
-let colorElement1 = document.getElementById("status");
-let colorElement2 = document.getElementById("status1");
+let colorElement1 = document.getElementById("status1");
+let colorElement2 = document.getElementById("status2");
+let device, location;
 
 function main() {
     console.log('Page is fully loaded');
@@ -14,24 +16,121 @@ colorElement2.addEventListener('click', onClickSquareBox2);
 colorElement2.addEventListener('touch', onClickSquareBox2);
 
 
-
 async function onClickSquareBox1() {
-
-    data.forEach(function(value){
-        document.getElementById("status").innerHTML = "This is color Brown.";
-        let utterance = new SpeechSynthesisUtterance(value.status_message);
-        speechSynthesis.speak(utterance);
-    });
+    location = locationsArray[0];
+    let confirmation = "Treasure ready: " + location.name;
+    document.getElementById("status1").innerHTML = confirmation;
+    let utterance = new SpeechSynthesisUtterance(confirmation);
+    console.log("hh")
+    speechSynthesis.speak(utterance);
 }
 
 async function onClickSquareBox2() {
-    
-    data.forEach(function(value){
-        document.getElementById("status1").innerHTML = value.status1_message;
-        let utterance = new SpeechSynthesisUtterance(value.status1_message);
-        speechSynthesis.speak(utterance);
+    device = await getLocation();
+
+    let isInside = isInsideQuad(device, location);
+    let status;
+    if(isInside) {
+        status = "You have reached " + location.name;
+    } else {
+        status = "You haven't reached the quest";
+    }
+    document.getElementById("status2").innerHTML = status;
+    let utterance = new SpeechSynthesisUtterance(status);
+    speechSynthesis.speak(utterance);
+
+}
+
+// collects current location
+async function getLocation() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    }).then(position => {
+        return position;
     });
 }
+
+function isValid(coordinates) {
+    let lat = coordinates.latitude;
+    let lon = coordinates.longitude;
+
+    if (lat < -90 || lat > 90 || lon < -180 || lon > 180)
+        return false;
+    else
+        return true;
+}
+
+function isValidDevice(device) {
+    let deviceCoordinates = {};
+    deviceCoordinates["latitude"] = device.coords.latitude;
+    deviceCoordinates["longitude"] = device.coords.longitude;
+
+    if (isValid(deviceCoordinates))
+        return true;
+    else
+        throw "Invalid Device";
+}
+
+function isValidType(location) {
+    if (location.type === "quad")
+        return true;
+    else
+        throw "Invalid Location Type";
+}
+
+function isValidCoordinates(coordinates) {
+    if (coordinates.length != 4)
+        return false;
+
+    coordinates.forEach(function (coordinate, index) {
+        if (!isValid(coordinate))
+            return false;
+    })
+
+    return true;
+}
+
+function isValidLocation(location) {
+    if (location.name.length > 0 && isValidType(location) && isValidCoordinates(location.coordinates))
+        return true;
+    else
+        throw "Invalid Location";
+}
+
+function isValidArguments(device, location) {
+    if(device == null && location == null)
+        throw "Two valid arguments are needed";
+    else
+        return true;
+}
+
+function isInsideQuad(device, location) {
+    try {
+        let checkValid = isValidArguments(device, location) && isValidDevice(device) && isValidType(location) && isValidLocation(location);
+        if (checkValid) {
+            let x = device.coords.latitude;
+            let y = device.coords.longitude;
+
+            let inside = false;
+            let coordinates = location.coordinates;
+            for (let i = 0, j = coordinates.length - 1; i < coordinates.length; j = i++) {
+                let xi = coordinates[i]["latitude"], yi = coordinates[i]["longitude"];
+                let xj = coordinates[j]["latitude"], yj = coordinates[j]["longitude"];
+
+                let intersect = ((yi > y) != (yj > y))
+                    && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+                if (intersect) inside = !inside;
+            }
+            return inside;
+        }
+
+    } catch (err) {
+        console.log("Exception: " + err);
+    }
+}
+
+
+
 
 
 
