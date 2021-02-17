65620dcb8fa89d4ea9dc4994c653b57530ee10e3
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-02-17 07:39:55 -0600

    created location.js

---
+++ scripts/location.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        name: '1213 Apartments',
+        type: 'quad',
+        coordinates: [
+            {
+                latitude: 40.360761,
+                longitude: -94.888086
+            },
+            {
+                latitude: 40.360743,
+                longitude: -94.887509
+            },
+            {
+                latitude: 40.360582,
+                longitude: -94.887525
+            },
+            {
+                latitude: 40.360627,
+                longitude: -94.888096
+            }
+        ]
+    }
+
+];
