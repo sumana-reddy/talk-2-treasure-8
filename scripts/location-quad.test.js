e66b2f4b2fa83f1f7f7bd6f34b402343a2bf9f9c
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-02-19 09:53:17 -0500

    added a description for test function

--- scripts/location-quad.test.js
+++ scripts/location-quad.test.js
@@ -1,1 +1,15 @@
 // test function will be here
+
+// need to add the valid tests for location
+
+import isValidCoordinates from './main.js';
+
+
+//negative test case that checks when the given co-ordinates are null 
+// and should return false.
+
+describe('isValidCoordinates function', () => {
+    it('checks whether the coordinates are valid', () => {
+      expect(isValidCoordinates({})).toEqual(false);
+    });
+  });
