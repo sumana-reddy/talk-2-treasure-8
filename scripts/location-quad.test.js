7210e1876d682f8df9d6966aca882490d1137eec
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-02-22 10:02:39 -0500

    added some code and commented

--- scripts/location-quad.test.js
+++ scripts/location-quad.test.js
@@ -13,3 +13,45 @@ describe('isValidCoordinates function', () => {
       expect(isValidCoordinates({})).toEqual(false);
     });
   });
+
+/*
+
+// location-Quad.js
+
+const isCoordinateValid = (coord) => {
+  // check latitude values are in range
+  // check longitude values are in range
+  return true;
+}
+
+const isDeviceValid = (dev) => {
+  // check if dev.coordinate
+   // check dev.coordinate with isCoordinateValid
+  return true;
+}
+
+const isLocationValid = (loc) => {
+  // check if loc.coordinates array exists
+  // check if loc.coordinates array length is correct for shape
+  // check each coordinate with isCoordinateValid
+  return true;
+}
+
+
+const isInsideQuad = (device, location) => {
+  // check args
+  const isDeviceValid = isDeviceValid(device);
+  const isLocationValid = isLocationValid(location);
+
+  if (!isDeviceValid) {
+    throw new Error('Invalid device');
+  }
+
+  if (!isLocationValid) {
+    throw new Error('Invalid location');
+  }
+
+  return true;
+};
+
+*/
