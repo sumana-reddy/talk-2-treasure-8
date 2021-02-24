edbbe2b53447e267588a49592f0de0797fb5f35f
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-02-24 10:04:09 -0500

    changed the test code

--- scripts/location-quad.test.js
+++ scripts/location-quad.test.js
@@ -1,8 +1,7 @@
 // test function will be here
 
-// need to add the valid tests for location
-
 import isValidCoordinates from './main.js';
+const loc_quad = require('./main');
 
 
 //negative test case that checks when the given co-ordinates are null 
@@ -11,6 +10,14 @@ import isValidCoordinates from './main.js';
 describe('isValidCoordinates function', () => {
     it('checks whether the coordinates are valid', () => {
       expect(isValidCoordinates({})).toEqual(false);
+    });
+  });
+
+  //test case to check whether the get location function returns undefined if not inside the Quad.
+
+  describe('getLocation function', () => {
+    it('checks whether the getLocation function returns the position', () => {
+      expect(loc_quad.getLocation).toEqual(undefined);
     });
   });
 
